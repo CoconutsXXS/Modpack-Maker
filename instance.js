bc3c93c90711af4f8e6869fd1f745f3c0c05f91d
Coconuts XXS <123900385+CoconutsXXS@users.noreply.github.com>
2025-10-12 16:48:46 +0200

    Update instance.js

--- instance.js
+++ instance.js
@@ -336,14 +336,6 @@ class Instance
             // quickPlay: {type: "singleplayer", identifier: "Structure Edition"}
             quickPlay: world!=undefined?world:null,
             javaPath: javaPath?javaPath:'java',
-<<<<<<< Updated upstream
-=======
-
-            features: ["is_demo_user", "offline"],
-
-            detached: true,
-            customArgs: ['-XstartOnFirstThread'],
->>>>>>> Stashed changes
             overrides: {detached: true}
         }
         console.log(options)
